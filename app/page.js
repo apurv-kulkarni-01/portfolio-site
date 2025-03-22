8d054953b39475ef3a4ecf0d58e9300251f80f3c
Apurv Kulkarni <apurv@appectual.com>
2025-03-22 15:18:07 +0530

    Added nav Links; updated layout.js and learnt Next special files; @ used for importing from root directory

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
+import Footer from "@/components/footer";
+
 export default function Home() {
   return (
     <main>
       <h1>👮Apurv Kulkarni👮</h1>
-      <p>🚨🚔the code police🚨🚔</p>
+      <p>welcome to my portfolio website</p>
+      <p><Link href="/about-us">About Us</Link></p>
+      <p><Link href="/contact-us">Contact Us</Link></p>
+      <Footer />
     </main>
   );
 }
